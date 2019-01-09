77cd96f47953195a2114d1d6eb68af30b8357acc
Dogan Pamuklu <dogan.pamuklu@accenture.com>
2019-01-09 12:00:07 +0200

    port checking

--- fpl/app.js
+++ fpl/app.js
@@ -91,5 +91,5 @@ function round(value, decimals) {
     return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals).toFixed(decimals);
 };
 
-app.listen(process.env.PORT, function() {
+app.listen(process.env.PORT || 3000, function() {
 console.log("SERVER IS RUNNING")});
